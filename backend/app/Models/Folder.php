9eb93be0632e86e1fa708ce107512f765024205c
Amir Hemmateenejad <ahp@mail.ir>
2026-04-23 20:13:03 +0330

    add soft delete

--- backend/app/Models/Folder.php
+++ backend/app/Models/Folder.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Folder extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'project_id',
         'parent_id',
@@ -32,5 +34,20 @@ class Folder extends Model
     public function snippets(): HasMany
     {
         return $this->hasMany(Snippet::class);
+    }
+
+    protected static function booted()
+    {
+        static::deleting(function ($folder) {
+            if (!$folder->isForceDeleting()) {
+                $folder->children()->each->delete();
+                $folder->snippets()->each->delete();
+            }
+        });
+
+        static::restoring(function ($folder) {
+            $folder->children()->onlyTrashed()->each->restore();
+            $folder->snippets()->onlyTrashed()->each->restore();
+        });
     }
 }
--- backend/app/Models/Snippet.php
+++ backend/app/Models/Snippet.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Snippet extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'project_id',
         'folder_id',
@@ -28,5 +30,20 @@ class Snippet extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'snippet_tag');
+    }
+
+    protected static function booted()
+    {
+        static::deleting(function ($project) {
+            if (!$project->isForceDeleting()) {
+                $project->folders()->each->delete();
+                $project->snippets()->each->delete();
+            }
+        });
+
+        static::restoring(function ($project) {
+            $project->folders()->onlyTrashed()->each->restore();
+            $project->snippets()->onlyTrashed()->each->restore();
+        });
     }
 }
